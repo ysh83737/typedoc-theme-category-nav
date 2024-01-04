88208e49f7e5945feaf67ee0c33c1c64d9c3668b
ShawnYuan <ysh83737@hotmail.com>
2024-01-04 14:18:37 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+};
